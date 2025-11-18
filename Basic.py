c560ecf74b54a29a299fa97073ef56170916ddce
Rohit <rohitmali9900@email.com>
2025-11-18 15:51:46 +0530

    Added all python files

---
+++ Basic.py
@@ -0,0 +1,17 @@
+# Variables
+bus_cost = 20
+place = "BTM"
+isBUS = True
+
+software = ["vscode", "jupyter", "anaconda"]   # list
+languages = ("Python", "Java", "HTML", "CSS", "JS")  # tuple
+
+dictionary = {"name": "XYZ", "age": 25}  # dictionary
+
+print("Todays bus cost is " ,bus_cost, "from" ,place,"through Bus",isBUS)
+print(software, languages, dictionary)
+
+for soft in software:
+    print(soft)
+
+print("Git connection working!")
